533cc10213cb4d0c761bfbdc3e52798cb8be3292
jairo <jairocostanascimento@gmail.com>
2025-11-09 00:38:17 -0300

    inicio

--- tela_login.py
+++ tela_login.py
@@ -1,52 +1,90 @@
-#tela de login
 import customtkinter as ctk
+from PIL import Image
 
 
-
+#configurações da tela
 tela_loguin=ctk.CTk()
 tela_loguin.title("Locadora")
 tela_loguin.geometry("800x600")
-tela_loguin.configure(fg_color="#1e1b3a")
+tela_loguin.configure(fg_color="black")
 tela_loguin.resizable(False,False)
 
-texto_titulo=ctk.CTkLabel(tela_loguin,text="Locadora",text_color="white",font=("Verdana",40,"bold"))
-texto_titulo.place(x=350,y=150)
 
-texto_subtitulo=ctk.CTkLabel(tela_loguin,text="Bem vindo",text_color="white",font=("verdana",44,"bold"))
-texto_subtitulo.place(x=300,y=250)
+#card da tela
+card=ctk.CTkFrame(tela_loguin,corner_radius=15,
+                  fg_color="#3a3d7b",
+                  width=270,
+                  height=588)
+card.place(x=6,y=6)
 
-entrada_usuario=ctk.CTkEntry(tela_loguin,
+
+#caminho e tamanho da imagem
+fundo=ctk.CTkImage(Image.open("imagens/fundo.png"),
+                   size=(524,589))
+
+#carregar a imagem
+label_fundo=ctk.CTkLabel(tela_loguin,image=fundo,text="")
+label_fundo.place(x=290,y=6)
+
+
+
+#texto do titulo
+texto_titulo=ctk.CTkLabel(card,text="BEM VINDO! ",text_color="white",font=("Achirvo black",28,"bold"))
+texto_titulo.place(x=50,y=200)
+
+#texto subtitulo
+texto_subtitulo=ctk.CTkLabel(card,text="faça seu login para continuar",text_color="white",font=("verdana",10,"bold"))
+texto_subtitulo.place(x=55,y=228)
+
+
+#entrada para usuário
+entrada_usuario=ctk.CTkEntry(card,
                              placeholder_text="Usuario "
                              ,text_color="white"
                              ,width=250,
                              height=40,
-                             corner_radius=10,
+                             corner_radius=30,
                              border_width=2,
-                             border_color="#8b5cf6",
-                             fg_color="#1e1b3a")
+                             border_color="",
+                             fg_color="#202244")
+entrada_usuario.place(x=10,y=270)
 
-entrada_usuario.place(x=300,y=350)
 
+#entrada para senha
+entrada_senha=ctk.CTkEntry(card,
+                             placeholder_text="Senha"
+                             ,text_color="white"
+                             ,width=250,
+                             height=40,
+                             corner_radius=30,
+                             border_width=2,
+                             border_color="",
+                             fg_color="#202244")
+entrada_senha.place(x=10,y=320)
+
+
+#butão pra criar conta
 butao_criarconta=ctk.CTkButton(
-                    tela_loguin,
+                    card,
                     text="criar conta",
                     width=120,
                     height=35,
-                    corner_radius=10,
-                    border_color="#8b5cf6",
-                    fg_color="#3b3570")
-butao_criarconta.place(x=300,y=410)
+                    corner_radius=30,
+                    border_color="",
+                    fg_color="#202244")
+butao_criarconta.place(x=10,y=370)
 
-butao_entrar=ctk.CTkButton(tela_loguin,
+
+#butão para entrar
+butao_entrar=ctk.CTkButton(card,
                            text="entrar",
                            width=120,
                            height=35,
-                           corner_radius=10,
-                           border_color="#8b5cf6",
-                           fg_color="#3b3570")
-butao_entrar.place(x=427,y=410)
+                           corner_radius=30,
+                           border_color="",
+                           fg_color="#202244")
+butao_entrar.place(x=140,y=370)
 
 
-
-
+#inicia a tela
 tela_loguin.mainloop()
