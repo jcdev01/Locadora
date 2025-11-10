268ec6c5a5136a4bab318e6f2e3f60dd25c1295c
LukBomfim <lucasbomfim2801@gmail.com>
2025-11-10 16:14:16 -0300

    Add Placa e toString

--- carro.py
+++ carro.py
@@ -1,8 +1,19 @@
-class Carro():
-    def __init__(self, marca, modelo, ano, cambio, portas, diaria):
+class Carro:
+    total = 0
+    disponiveisAgora = 0
+
+    def __init__(self, marca, modelo, ano, cambio, portas, placa, diaria):
         self.marca = marca
         self.modelo = modelo
         self.ano = ano
         self.cambio = cambio
         self.portas = portas
+        self.placa = placa
         self.diaria = diaria
+
+        Carro.total += 1
+        Carro.disponiveisAgora += 1
+        print(f"Novo veículo cadastrado: \n"
+              f"    Marca: {marca}\n"
+              f"    Modelo/ano: {modelo} {ano}\n"
+              f"    Placa: {placa}")
