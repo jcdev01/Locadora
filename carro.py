09ade11b5ba76732132027035124788516a47ac9
LukBomfim <lucasbomfim2801@gmail.com>
2025-11-10 12:35:15 -0300

    Criacao da classe Carro

--- carro.py
+++ carro.py
@@ -1,8 +1,8 @@
 class Carro():
-    def __init__(self, marca, modelo, ano, cambio, diaria):
+    def __init__(self, marca, modelo, ano, cambio, portas, diaria):
         self.marca = marca
         self.modelo = modelo
         self.ano = ano
         self.cambio = cambio
+        self.portas = portas
         self.diaria = diaria
-
